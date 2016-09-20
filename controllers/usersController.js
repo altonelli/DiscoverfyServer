0514bf7a319fcb86ab55f625de359a5c61c19c33
altonelli <altonelli14@gmail.com>
2016-09-19 18:52:45 -0700

    creating username based off of req.body.user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,14 @@
 var db = require('../models');
 
 function create(req,res){
-  console.log("req.body.name " + req.body.user);
+  console.log("req.body.user " + req.body.user);
   db.User.findOne({name:req.body.user}).exec(function (err, user){
     if (user) {
       console.log("here is your user 1 " + user);
       res.status(200).json(user);
     } else {
       var newUser = new db.User({
-        name: req.body.name,
+        name: req.body.user,
         songs: []
       });
       newUser.save(function(err,user){

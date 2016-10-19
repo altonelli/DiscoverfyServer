c08b2e6a35f852ed3e722736083a22d9c37e99af
altonelli <altonelli14@gmail.com>
2016-10-19 16:59:57 -0700

    added postinstall to package json

--- util/encryption.js
+++ util/encryption.js
@@ -12,7 +12,6 @@ function encrypt(text){
 }
 
 function decrypt(text){
-  console.log('decrypt called');
   var decipher = crypto.createDecipher('aes-256-ctr',encryption_secret);
   var dec = decipher.update(text,'hex','utf8');
   dec += decipher.final('utf8');

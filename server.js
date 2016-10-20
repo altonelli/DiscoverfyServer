fca81b05d93c9eda8f6878244f90a41dee18088e
altonelli <altonelli14@gmail.com>
2016-10-19 17:52:56 -0700

    trying to remove .env for build

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var env = require("node-env-file");
-env(__dirname + '/.env');
+// var env = require("node-env-file");
+// env(__dirname + '/.env');
 
 var express = require("express");
 var bodyParser = require("body-parser");
